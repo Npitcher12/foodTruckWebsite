713af02f27f155476859d7a0b02a257525c9fdf2
Npitcher12 <63477243+Npitcher12@users.noreply.github.com>
2021-12-05 22:58:31 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -1,25 +1,3 @@
-function checkEmail() {
-	var email = document.getElementById('email');
-	var text = email.value;
-	var feedback = document.getElementById('wrongEmail');
-	if (text.indexOf('@') < 0) {
-		feedback.innerHTML = "Please enter a valid email";
-	} else {
-		feedback.innerHTML = "";
-	}
-}
-
-function checkMessage() {
-	var msg = document.getElementById('message');
-	var text = msg.value;
-	var feedback = document.getElementById('wrongMessage');
-	if (text.length <= 5) {
-		feedback.innerHTML = "Message must be longer than 5 characters";
-	} else {
-		feedback.innerHTML = "";
-	}
-}
-
 function photoPopup() {
 	const photoPopup = document.getElementById('photoPopup');
 	photoPopup.style.display = "block"
@@ -83,14 +61,6 @@ function checkStars(className) {
 		star5.classList.add("checked");
 	}
 }
-
-
-var email = document.getElementById('email');
-var msg = document.getElementById('message');
-
-
-email.addEventListener('keyup', checkEmail, false);
-msg.addEventListener('keyup', checkMessage, false);
 
 
 
